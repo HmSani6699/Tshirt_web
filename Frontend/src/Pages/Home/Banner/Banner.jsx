7cb0946377841dab39cddd5d7e0a13c945da1e1f
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-05-24 11:53:07 +0600

    added  navbar and banner

--- Frontend/src/Pages/Home/Banner/Banner.jsx
+++ Frontend/src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import banner_image_1 from "./../../../../src/assets/images/1.jpg";
+import banner_image_2 from "./../../../../src/assets/images/2.jpg";
+import banner_image_3 from "./../../../../src/assets/images/3.jpg";
 
 const Banner = () => {
   return (
-    <div>
-      <h2>Banner component</h2>
+    <div className="bg-[#eeeeee]  w-full">
+      <div>
+        <Swiper
+          // spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          // navigation={true}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={banner_image_1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={banner_image_2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={banner_image_3} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
--- Frontend/src/Pages/Home/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navber from "../../../Navber/Navber";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
       <Navber />
+      <Banner />
     </div>
   );
 };
