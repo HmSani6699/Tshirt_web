9a04bcef0c84106f156ccb0b37a2abd3b742869b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2024-05-25 13:15:32 +0600

    added new cart design

--- Frontend/src/component/GalleryCart/GalleryCart.jsx
+++ Frontend/src/component/GalleryCart/GalleryCart.jsx
@@ -1,15 +1,39 @@
 import React from "react";
 import cart_image from "../../../src/assets/images/cart/cart.webp";
-import { MdOutlineStar } from "react-icons/md";
-import { MdOutlineStarHalf } from "react-icons/md";
-import { MdOutlineStarBorder } from "react-icons/md";
+import {
+  MdOutlineStar,
+  MdOutlineStarHalf,
+  MdOutlineStarBorder,
+} from "react-icons/md";
+import { GoHeart, GoLock } from "react-icons/go";
+import { SlEye } from "react-icons/sl";
 
 const GalleryCart = () => {
   return (
     <div className="border rounded-md">
       {/* Cart image */}
-      <div className="h-[300px]">
-        <img className="h-full w-full rounded-md" src={cart_image} alt="" />
+      <div className="h-[300px] bg-[#eeeeee] relative group">
+        <img
+          className="h-full w-full rounded-md"
+          src={cart_image}
+          alt="Cart item"
+        />
+        {/* Hover content */}
+        <div className="absolute inset-0 bg-[#cbc9c9a2] transition-opacity duration-500 opacity-0 group-hover:opacity-100 z-10 flex flex-col items-end gap-[15px] pt-[10px] pr-[10px]">
+          <div className="h-[35px] w-[35px] cursor-pointer shadow-md hover:bg-red-400 bg-white flex items-center justify-center rounded-full">
+            <GoHeart className="hover:text-white" />
+          </div>
+          <div className="h-[35px] w-[35px] cursor-pointer shadow-md hover:bg-red-400 bg-white flex items-center justify-center rounded-full">
+            <SlEye className="hover:text-white" />
+          </div>
+          <div className="h-[35px] w-[35px] cursor-pointer shadow-md hover:bg-red-400 bg-white flex items-center justify-center rounded-full">
+            <GoLock className="hover:text-white" />
+          </div>
+        </div>
+        {/* Discount div */}
+        <div className="absolute top-[20px] left-0 w-[60px] pl-[10px] rounded-r-full bg-red-400 text-white">
+          <small>-70%</small>
+        </div>
       </div>
       {/* Cart text content */}
       <div className="text-center">
@@ -24,7 +48,7 @@ const GalleryCart = () => {
             <MdOutlineStarBorder className="text-red-400" />
           </div>
         </div>
-        <h2 className="font-semibold mt-[6px]"> $20.00</h2>
+        <h2 className="font-semibold mt-[6px]">$20.00</h2>
       </div>
     </div>
   );
